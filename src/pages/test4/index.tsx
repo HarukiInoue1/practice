8c3a7ca2354d73f3eb4619a46958605bdc0a153c
HarukiInoue1 <inoueh@tanapism.co.jp>
2024-07-21 19:37:52 +0900

    react-query practice

--- src/pages/test4/index.tsx
+++ src/pages/test4/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useState } from "react";
+// import { useState } from "react";
 import { useQuery } from "react-query";
 
 type Person = { name: string; age: number; note: string; registerDate: string };
@@ -9,28 +9,28 @@ type Person = { name: string; age: number; note: string; registerDate: string };
 const Test4: NextPage = () => {
   const router = useRouter();
 
-  const [personList, setPersonList] = useState<Person[]>([]);
-
-  const { isLoading, error } = useQuery("repoData", () =>
-    axios
-      .get("https://umayadia-apisample.azurewebsites.net/api/persons")
-      .then((res) => setPersonList(res.data.data))
-      .catch((err) => console.log(err))
-  );
+  const getPersonList = async () => {
+    const res = await axios.get(
+      "https://umayadia-apisample.azurewebsites.net/api/persons"
+    );
+    return res.data.data;
+  };
+  
+  const {data, isLoading, error,isSuccess} = useQuery<Person[]>("persons", getPersonList);
 
   // ローディング中
   if (isLoading) return <div>ろーどちゅう</div>;
 
   // エラー
   if (error) return <div>エラー</div>;
-
-  console.log(personList);
-
+  console.log(isSuccess);
   return (
     <>
       <div>
         <button onClick={() => router.push("/")}>TOPページへ</button>
-        {personList.map((elem, index) => (
+        {/* <div>{isSuccess ? "データの取得に成功しました" : "データの取得に失敗しました"}</div> */}
+        {/* useQueryから取得するdataの型はisLoading、errorも含むため、Person[] | undefinedとなる？*/}
+        {data?.map((elem, index) => (
           <div key={index}>
             <p>名前：{elem.name}</p>
             <p>年齢：{elem.age}</p>
